004f3be4706ac5c91f432229dc89cf57312fe9b4
John Johnson Mbeba <john.mbeba@student.moringaschool.com>
2022-08-29 14:18:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+class Polygon{
+    constructor(array){
+        this.array = array;
+    }
+
+    //Get methods
+    get countSides(){
+        return this.array.length;
+    }
+
+    get perimeter(){
+        return this.array.reduce((prev, curr)=> {
+            return prev + curr;
+        })
+    }
+}
+
+class Triangle extends Polygon{
+    get isValid(){
+        if(this.array[0]+this.array[1] > this.array[2]){
+            if(this.array[0]+this.array[2] > this.array[1]){
+                if(this.array[2]+this.array[1] > this.array[0]){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+}
+
+class Square extends Polygon{
+    get isValid(){
+        return this.perimeter === this.array[0]*4;
+    }
+
+    get area(){
+        return Math.pow(this.array[0],2)
+    }
+}
